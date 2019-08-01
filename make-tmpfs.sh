aadd953c93aef2d896e0301913d0a01c5ba27d65
nsv <nsv@valk.ws>
2019-08-01 12:47:22 +0200

    Update tmpfs

--- make-tmpfs.sh
+++ make-tmpfs.sh
@@ -2,5 +2,5 @@
 # Make /tmp a tmpfs
 
 if ! grep -q '^tmpfs\s\+/tmp\s' /etc/fstab; then
-    echo "tmpfs /tmp tmpfs rw,nosuid,nodev" | sudo tee -a /etc/fstab
+    echo "tmpfs /tmp tmpfs rw,nosuid,nodev,noatime,mode=1777 0 0" | sudo tee -a /etc/fstab
 fi
