eac1c9f99cd57fe03c2e85f6b27130f29fda7c76
nsv <nsv@valk.ws>
2019-08-02 19:06:47 +0200

    Add aerc

---
+++ app/aerc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# aerc - An email client
+# Needs golang too
+
+INSTALL="${LOCALBUILDS:-$HOME/.local-builds}/aerc"
+wget -O /tmp/aerc.tar.gz "https://git.sr.ht/~sircmpwn/aerc/archive/0.2.1.tar.gz"
+mkdir -p "$INSTALL"
+tar xvf /tmp/aerc.tar.gz -C"$INSTALL"
+cd "$INSTALL/aerc-0.2.1"
+sudo apt install dante-client w3m python3 python3-colorama
+make
+sudo make install
