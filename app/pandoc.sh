61fa882143bf63af2fc859957698a6e256eaa21e
nsv <nsv@valk.ws>
2019-09-04 13:02:00 +0200

    Add pandoc download script

---
+++ app/pandoc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Pandoc. The version in the debian repos is outdated
+
+wget -O /tmp/pandoc.deb "$( \ 
+    curl -s 'https://api.github.com/repos/jgm/pandoc/releases/latest' \
+    | jq -r '.assets | .[] | .browser_download_url | select(endswith("amd64.deb"))'\
+)"
+
+sudo dpkg -i /tmp/pandoc.deb
+
