f705ece7dd2ffe0da25bd5f78e1f01584bdc3646
nsv <nsv@valk.ws>
2019-09-04 15:26:47 +0200

    Add highlighting for jq.

--- app/vim-plugins.sh
+++ app/vim-plugins.sh
@@ -7,6 +7,7 @@ git clone https://github.com/jamessan/vim-gnupg.git # Open encrypted files
 git clone https://github.com/vim-syntastic/syntastic.git # Check code syntax
 git clone https://github.com/bitc/vim-hdevtools.git # Interactive Haskell development
 git clone https://github.com/vim-pandoc/vim-pandoc-syntax.git # Highlight markdown
+git clone https://github.com/vito-c/jq.vim # Highlight jq files
 git clone https://github.com/ledger/vim-ledger.git # ledger/hledger journal writing
 git clone https://github.com/mhinz/vim-signify # Show git or svn changes
 git clone https://github.com/tpope/vim-fugitive.git # Git wrapper to see blame etc
