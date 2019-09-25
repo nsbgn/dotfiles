65949508ff3cea26bd53cd3848a87bc1b34eaba8
nsv <nsv@valk.ws>
2019-09-25 16:16:53 +0200

    Update firejail config
    Will need more changes, probably. It's a bit frustrating at the moment
    due to different openers (xdg-open, $BROWSER etc) doing different things
    and getting confused in their sandbox. Like, opening a link through
    Telegram makes it throw a fit.

--- setup/firejail.sh
+++ setup/firejail.sh
@@ -17,16 +17,39 @@ apparmor
 private $SANDBOX_HOME
 EOF
 
-# Make sure that the symlink is the first one that will be found in the PATH
+for $PROGRAM in telegram transmission-gtk; do
+sudo tee /etc/firejail/$PROGRAM.local << EOF
+join-or-start $PROGRAM
+EOF
+
+
 # Setting a symlink to firejail with the name of the program will run the
-# program in the fire
-# Note that you could also just run `firecfg` to link *all* programs for which
-# firejail has a profile.
-ln -s /usr/bin/firejail /usr/local/bin/firefox
+# program in the firejail. Make sure that the symlink is the first one that
+# will be found in the PATH. Firecfg automates this. We add firefox manually,
+# because it won't be found in /opt
+sudo firecfg
+sudo ln -s /usr/bin/firejail /usr/local/bin/firefox
+sudo rm /usr/local/bin/mpv
 
 # Note that `xdg-settings get default-web-browser` indicated I had some
 # .desktop file in my `.local/share/applications` that was circumventing
 # firejail when opened in some ways, so I had to delete that.
+
+tee .local/share/applications/firefox.desktop << EOF
+[Desktop Entry]
+Name=firefox
+GenericName=Web Browser
+Icon=firefox
+Type=Application
+Categories=Network;WebBrowser;
+Exec=firefox %u
+Terminal=false
+StartupNotify=false
+MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;
+Keywords=Browser
+Actions=new-window;preferences;
+EOF
+xdg-settings get default-web-browser firefox
 
 # There should be a new page that I can set for new windows on which Vim Vixen
 # will work
