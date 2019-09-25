bfa3d67691188bdb18331dae16ff87dbae211eec
nsv <nsv@valk.ws>
2019-09-25 17:08:47 +0200

    Change to vim vixen in addon script too

--- app/firefox-addons.sh
+++ app/firefox-addons.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for ADDON in umatrix ublock-origin decentraleyes vimium-ff; do
+for ADDON in umatrix ublock-origin decentraleyes vim-vixen; do
     wget -O /tmp/$ADDON.xpi https://addons.mozilla.org/firefox/downloads/latest/$ADDON/addon-$ADDON-latest.xpi
 done
-firefox /tmp/umatrix.xpi /tmp/ublock-origin.xpi /tmp/decentraleyes.xpi /tmp/vimium-ff.xpi
+firefox /tmp/umatrix.xpi /tmp/ublock-origin.xpi /tmp/decentraleyes.xpi /tmp/vim-vixen.xpi
