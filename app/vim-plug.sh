aa37ef4240f0ec3ad1bea92f8bb5395dc9660553
nsv <nsv@valk.ws>
2019-11-19 22:21:16 +0100

    Switch to neovim

--- app/vim-plug.sh
+++ app/vim-plug.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Vim plugins
 
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
