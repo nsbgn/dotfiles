d3badf8305928b156556550aaadd648cb38585f4
nsv <nsv@valk.ws>
2019-12-09 21:07:55 +0100

    Add meli email client

---
+++ app/meli.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# meli - Email client
+# https://meli.delivery/posts/2019-12-09-alpha-release.html
+
+INSTALL="${LOCALBUILDS:-$HOME/.local-builds}/meli"
+git clone https://git.meli.delivery/meli/meli.git "$INSTALL"
+cd "$INSTALL"
+make
+# try without installing:
+# ./target/release/meli
+#make PREFIX=$HOME/.local install 
