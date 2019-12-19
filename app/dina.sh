77f17f16c3c4385abc00795c43c5f823a8b8778a
nsv <nsv@valk.ws>
2019-12-19 19:10:18 +0100

    Update Dina installation script

--- app/dina.sh
+++ app/dina.sh
@@ -3,8 +3,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-wget -O Dina.zip "https://www.dcmembers.com/jibsen/download/61/?wpdmdl=61"
-sudo unzip -d /usr/share/fonts/Dina Dina.zip
-cd /usr/share/fonts/Dina/BDF && sudo mkfontscale && sudo mkfontdir
+wget -O /tmp/Dina.zip "https://www.dcmembers.com/jibsen/download/61/?wpdmdl=61"
+sudo unzip -d /usr/share/fonts/Dina /tmp/Dina.zip
+cd /usr/share/fonts/Dina/BDF
+sudo mkfontscale
+sudo mkfontdir
 sudo dpkg-reconfigure fontconfig-config
 fc-cache -f
