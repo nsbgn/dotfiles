56ffb88408b9c8c2c56bcecd99bf51d05a1cd9e0
nsv <nsv@valk.ws>
2019-12-19 19:13:26 +0100

    Update kiwix install script

--- app/kiwix.sh
+++ app/kiwix.sh
@@ -3,6 +3,5 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-INSTALL="${LOCALBUILDS:-$HOME/.local-builds}/kiwix"
-wget -O "$INSTALL/kiwix-desktop.appimage" "https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_x86_64.appimage"
-chmod +x "$INSTALL/kiwix-desktop.appimage"
+/usr/bin/wget -O ~/.local/bin/kiwix-desktop "https://download.kiwix.org/release/kiwix-desktop/kiwix-desktop_x86_64.appimage"
+chmod +x ~/.local/bin/kiwix-desktop
