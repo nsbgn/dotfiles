3e201dca6cd3311b0b79260236d2f5bc486df2e1
nsv <nsv@valk.ws>
2019-12-23 20:06:33 +0100

    Firefox addons via addon page. Switch back to ESR.

--- app/firefox-addons.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for ADDON in umatrix ublock-origin decentraleyes vim-vixen; do
-    wget -O /tmp/$ADDON.xpi https://addons.mozilla.org/firefox/downloads/latest/$ADDON/addon-$ADDON-latest.xpi
-done
-firefox /tmp/umatrix.xpi /tmp/ublock-origin.xpi /tmp/decentraleyes.xpi /tmp/vim-vixen.xpi
--- app/firefox.sh
+++ app/firefox.sh
@@ -3,6 +3,13 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-wget -O /tmp/firefox.tar.bz2 --content-disposition "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US" \
-    && sudo rm -rf /opt/firefox \
-    && sudo tar xjf /tmp/firefox.tar.bz2 -C/opt
+sudo apt install firefox-esr
+
+# For the build directly from Mozilla:
+#wget -O /tmp/firefox.tar.bz2 --content-disposition "https://download.mozilla.org/?product=firefox-latest-ssl&os=linux64&lang=en-US" \
+#    && sudo rm -rf /opt/firefox \
+#    && sudo tar xjf /tmp/firefox.tar.bz2 -C/opt
+
+declare -a ADDONS=( umatrix ublock-origin decentraleyes vim-vixen )
+ADDONS=("${ADDONS[@]/#/'https://addons.mozilla.org/firefox/addon/'}")
+
