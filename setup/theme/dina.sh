f4ea77e405c94fd9c080cd3ffe3079829799a460
nsv <nsv@valk.ws>
2020-08-09 14:52:14 +0200

    Install Dina locally.

--- setup/theme/dina.sh
+++ setup/theme/dina.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 IFS=$'\n\t'
 
 wget -O /tmp/Dina.zip "https://www.dcmembers.com/jibsen/download/61/?wpdmdl=61"
-sudo unzip -d /usr/share/fonts/Dina /tmp/Dina.zip
-cd /usr/share/fonts/Dina/BDF
+unzip -d ~/.fonts/Dina /tmp/Dina.zip
+cd ~/.fonts/Dina/BDF
 sudo mkfontscale
 sudo mkfontdir
 sudo dpkg-reconfigure fontconfig-config
