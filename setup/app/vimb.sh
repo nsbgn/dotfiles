2d123f18a16b7846fc3f84e36815d7709bb22bda
nsv <nsv@valk.ws>
2021-03-19 13:25:08 +0100

    Add adblock to vimb

--- setup/app/vimb.sh
+++ setup/app/vimb.sh
@@ -8,3 +8,12 @@ git clone https://github.com/fanglingsu/vimb
 cd vimb
 make
 sudo make install
+
+cd -
+git clone https://github.com/jun7/wyebadblock
+cd wyebadblock
+make
+sudo make install
+sudo ln -s /usr/lib/wyebrowser/adblock.so /usr/local/lib/vimb/
+mkdir -p ~/.config/wyebadblock/
+wget -O ~/.config/wyebadblock/easylist.txt https://easylist.to/easylist/easylist.txt
