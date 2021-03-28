d18266e227705932dacf09ab5096432bdac8cbab
nsv <nsv@valk.ws>
2021-03-28 22:24:00 +0200

    Configure better

--- config/configure.sh
+++ config/configure.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -euo pipefail
 
-function link {
-    if [ -L "$2" ]; then # overwrite symbolic links but nothing else
-        ln -srTf "$1" "$2"
+for FILE in "$(dirname $0)"/*; do
+    NAME="$(basename $FILE)"
+    if [ -d "$FILE" -o "$NAME" == "user-dirs.dirs" ]; then
+        DEST="$HOME/.config/$NAME"
     else
-        ln -srT "$1" "$2"
+        DEST="$HOME/.$NAME"
     fi
-}
+    
+    echo "Linking $FILE to $DEST"
+    # overwrite symbolic links but nothing else
+    if [ -L "$DEST" ]; then
+        ln -srTf "$FILE" "$DEST"
+    else
+        ln -srT "$FILE" "$DEST"
+    fi
 
-for F in *; do
-    if [ "$F" == "configure.sh" ]; then
-        :
-    elif [ -d "$F" -o "$F" == "user-dirs.dirs" ]; then
-        link "$F" "$HOME/.config/$F"
-    elif [ -f "$F" ]; then
-        link "$F" "$HOME/.$F"
-    fi
 done
