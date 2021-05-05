3c1e4d35b29bae1ae18900c9142083192748c2db
nsv <nsv@valk.ws>
2021-05-05 09:55:26 +0200

    Add urxvt vim scrollback mode.

--- setup/apps/urxvt-font-size.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-git clone https://github.com/majutsushi/urxvt-font-size ~/.builds/urxvt-font-size
-mkdir -p ~/.urxvt/ext
-cp ~/.builds/urxvt-font-size/font-size ~/.urxvt/ext/
