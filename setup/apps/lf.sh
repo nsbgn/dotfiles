775dfc70686cc13ea8096b95955b8151c4fafb0a
nsv <nsv@valk.ws>
2021-05-16 15:53:45 +0200

    Use lf binary

--- setup/apps/lf.sh
+++ setup/apps/lf.sh
@@ -2,5 +2,13 @@
 # lf - File manager
 set -euo pipefail
 
-sudo apt install golang
-go get -u github.com/gokcehan/lf
+# sudo apt install golang
+# go get -u github.com/gokcehan/lf
+
+wget -O /tmp/lf.tar.gz "$( \
+    curl -s 'https://api.github.com/repos/gokcehan/lf/releases/latest' \
+    | jq -r '.assets | .[] | .browser_download_url | select(endswith("lf-linux-amd64.tar.gz"))'\
+)"
+
+mkdir -p ${HOME}/.local/bin
+cd ${HOME}/.local/bin && tar xvf /tmp/lf.tar.gz lf
