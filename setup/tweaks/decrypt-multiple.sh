26237143216ad83d22c6416dd680ea176f7565f4
nsv <nsv@valk.ws>
2021-05-17 16:03:59 +0200

    Multiple decryption in own script.

---
+++ setup/tweaks/decrypt-multiple.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# When you have have multiple encrypted drives that you'd like to unlock
+# simultaneously, you can do that by adding keyscript=decrypt_keyctl to both
+# drives in /etc/crypttab
+# - http://web.math.rochester.edu/people/faculty/akrish11//2018/04/28/debian-cryptroot.html
+# https://unix.stackexchange.com/questions/392284/using-a-single-passphrase-to-unlock-multiple-encrypted-disks-at-boot
+set -euo pipefail
+
+sudo apt install keyutils
+sudo sed --in-place 's/\(.+\)/\1,keyscript=decrypt_keyctl/g' /etc/crypttab
+sudo update-initramfs -u
