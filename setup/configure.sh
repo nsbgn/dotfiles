361543bcc26d3e7a8d078948544284aa77ef6e72
nsv <nsv@valk.ws>
2021-06-15 20:43:12 +0200

    Skip errors in configure.sh

--- setup/configure.sh
+++ setup/configure.sh
@@ -14,9 +14,9 @@ for FILE in "$(dirname $0)"/../config/*; do
     echo "$FILE -> $DEST"
     # overwrite symbolic links but nothing else
     if [ -L "$DEST" ]; then
-        ln -srTf "$FILE" "$DEST"
+        ln -srTf "$FILE" "$DEST" || :
     else
-        ln -srT "$FILE" "$DEST"
+        ln -srT "$FILE" "$DEST" || :
     fi
 
 done
