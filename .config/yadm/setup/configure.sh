759f4a286e8fc02067c153467a7b957b59b4b227
nsv <nsv@valk.ws>
2022-08-25 14:20:02 +0200

    Make `configure.sh` work

--- .config/yadm/setup/configure.sh
+++ .config/yadm/setup/configure.sh
@@ -2,7 +2,7 @@
 # Puts all configuration files in the right places.
 set -euo pipefail
 
-root="$(realpath $(dirname $0)/..)"
+root="$(realpath $(dirname $0)/../../..)"
 
 # echo "Copying keyd config"
 # sudo cp "$root/config/keyd/default.conf" /etc/keyd/default.conf
@@ -10,7 +10,7 @@ root="$(realpath $(dirname $0)/..)"
 
 mkdir -p ~/.local/share
 [ -L ~/.local/share/applications ] && \
-    ln -srTf $root/applications ~/.local/share/applications || \
+    ln -srTf $root/.local/share/applications ~/.local/share/applications || \
     :
 
 mkdir -p ~/.config
@@ -29,19 +29,10 @@ link(){
 
 for FILE in "$root"/.*; do
     if [ -f "$FILE" ]; then
-        NAME="$(basename $FILE)"
-        DEST="$HOME/$NAME"
-        link "$FILE" "$DEST"
+        link "$FILE" "$HOME/$(basename $FILE)"
    fi
 done
 
-
 for FILE in "$root"/.config/*; do
-    NAME="$(basename $FILE)"
-    if [ -d "$FILE" -o "$NAME" == "user-dirs.dirs" -o "$NAME" == "wayfire.ini" ]; then
-        DEST="$HOME/.config/$NAME"
-    else
-        DEST="$HOME/.$NAME"
-    fi
-    link "$FILE" "$DEST"
+    link "$FILE" "$HOME/.config/$(basename $FILE)"
 done
