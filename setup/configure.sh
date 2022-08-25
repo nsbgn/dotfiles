ee31bcee0172bd24def51b7d9a425cc604cee158
nsv <nsv@valk.ws>
2022-08-25 10:05:01 +0200

    Move files in preparation for bare git repo.

--- setup/configure.sh
+++ setup/configure.sh
@@ -14,7 +14,7 @@ mkdir -p ~/.local/share
     :
 
 mkdir -p ~/.config
-for FILE in "$root"/config/*; do
+for FILE in "$root"/.config/*; do
     NAME="$(basename $FILE)"
     if [ -d "$FILE" -o "$NAME" == "user-dirs.dirs" -o "$NAME" == "wayfire.ini" ]; then
         DEST="$HOME/.config/$NAME"
