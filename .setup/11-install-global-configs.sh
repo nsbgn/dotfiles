623dcc351314a81a6e8d64004e2a86b2678ebbc6
nsv <nsv@valk.ws>
2022-08-30 18:30:19 +0200

    Moving stuff around

--- .setup/11-install-global-configs.sh
+++ .setup/11-install-global-configs.sh
@@ -2,10 +2,11 @@
 # Install all files in $ROOT/.setup/etc/ to /etc/
 set -eu
 
-ROOT=$(dirname $0)/..
+ROOT="$(dirname $0)/.."
+ETC="$ROOT/.setup/etc"
 
-find "$ROOT/etc" -mindepth 2 -printf '%P\0'  -type f \
-| xargs -0 -I{} install -D -m644 "$ROOT/etc/{}" "/etc/{}"
+find "$ETC" -mindepth 2 -printf '%P\0'  -type f \
+| xargs -0 -I{} install -D -m644 "$ETC/{}" "/etc/{}"
 
 # Restart keyd
 # sudo systemctl restart keyd
--- .setup/debian/syncthing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This enables the Syncthing service. GUI on localhost:8384
-set -eu
-
-sudo apt install syncthing
-systemctl enable syncthing@$HOST.service
