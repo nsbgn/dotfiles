f118296cb04509ea1fc9a45c1c557f53bb4f509e
nsv <nsv@valk.ws>
2022-09-04 16:10:11 +0200

    Add WIP encryption scripts

--- .setup/02-encrypt-home-with-fscrypt.debian.sh
+++ .setup/02-encrypt-home-with-fscrypt.debian.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-# Enable file-level encryption on login using fscrypt. For device-level
-# encryption on Linux, use dm-crypt; for cross-platform device-level
-# encryption, use VeraCrypt.
+# Enable file-level encryption on login using `fscrypt`.
+#
+# For device-level encryption on Linux, use dm-crypt; for cross-platform
+# device-level encryption, use VeraCrypt.
 #
 # <https://wiki.debian.org/TransparentEncryptionForHomeFolder>
 # <https://www.rohanjain.in/fscrypt/>
@@ -10,51 +11,86 @@
 # <https://github.com/google/fscrypt>
 # <https://wiki.archlinux.org/title/Fscrypt>
 # <https://wiki.archlinux.org/title/PAM>
-#
-# gocryptfs:
-# <https://github.com/rfjakob/gocryptfs/wiki/Mounting-on-login-using-pam_mount>
+# <https://wiki.gentoo.org/wiki/PAM>
 
-set -e
+set -eu
 
-grep -q debian /etc/os-release || exit 1
+distro(){
+    grep -q "$1" /etc/os-release
+    exit $?
+}
 
-# sudo apt-get install gocryptfs libpam-mount
-sudo apt-get install fscrypt libpam-fscrypt
+if distro debian; then
+    sudo apt-get install fscrypt libpam-fscrypt
+elif distro alpine; then
+    cd
+    doas apk add git make gcc go m4 linux-pam-dev e2fsprogs-extra
+    git clone https://github.com/google/fscrypt
+    cd fscrypt
+    make
+    doas make install PREFIX=
+    alias sudo="doas"
 
-# Find device on which HOME is mounted
+    # Default login isn't linked to pam
+    doas apk add shadow-login
+else
+    exit 1
+fi
+
+# Make sure that $HOME is not yet encrypted
+fscrypt status "$HOME" || exit 1
+
+# Find device on which $HOME is mounted
 DEV=$(findmnt -n -o SOURCE --target "$HOME")
 
 # Make sure it is formatted as ext4
 test $(findmnt -n --source "$DEV" -o fstype) == "ext4" || exit 1
 
-# Enable `encrypt` feature flag on the device
+# Enable `encrypt` feature flag on the device and set up encryption
 sudo tune2fs -O encrypt "$DEV"
-
 sudo fscrypt setup
-# sudo setup $HOME # Only if not on root filesystem
+
+# Debian installs defaults for fscrypt here
+if distro debian && test -e /usr/share/pam-configs/fscrypt; then
+    sudo pam-auth-update
+
+elif distro alpine; then
+
+doas tee /etc/pam.d/fscrypt << EOF
+auth        required    pam_unix.so
 
 # Append this to the auth section
 sudo tee -a /etc/pam.d/system-login << EOF
 auth       optional   pam_fscrypt.so
 EOF
 
-# Insert this before `session include system-auth`
+# Insert this before `session include system-auth`. Note: The first line, taken
+# from https://github.com/google/fscrypt/issues/95, is a bypass for the systemd
+# --user session which doesn't properly close its PAM session and would
+# otherwise block the locking on logout.
 sudo tee -a /etc/pam.d/system-login << EOF
 session    [success=1 default=ignore]  pam_succeed_if.so  service = systemd-user quiet
 session    optional                    pam_fscrypt.so
 EOF
-# Note: The first line, taken from https://github.com/google/fscrypt/issues/95,
-# is a bypass for the systemd --user session which doesn't properly close its
-# PAM session and would otherwise block the locking on logout.
 
 sudo tee -a /etc/pam.d/passwd << EOF
 password    optional    pam_fscrypt.so
 EOF
 
-mkdir /home/$USER.crypt
-sudo chown $USER:$USER /home/$USER.crypt
-sudo fscrypt encrypt /home/$USER.crypt --user=$USER
-fscrypt status /home/newhome
+else
+    exit 1
+fi
+
+CRYPT=/home/$USER.crypt
+sudo mkdir $CRYPT
+sudo chown $USER:$USER $CRYPT
+sudo fscrypt encrypt $CRYPT --user=$USER --source=pam_passphrase
+fscrypt status $CRYPT
+
+# Make sure that $HOME is empty
+test -z "$(ls -A $HOME)" || exit 1
 
 # ... That's all.
 # fscrypt lock /home/$USER --user=$USER
+
+
--- .setup/02-encrypt-home-with-gocryptfs.alpine.sh
+++ .setup/02-encrypt-home-with-gocryptfs.alpine.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
+# Enable file-level encryption on login using gocryptfs
 # <https://wiki.archlinux.org/title/gocryptfs>
 # <https://github.com/rfjakob/gocryptfs/wiki/Mounting-on-login-using-pam_mount>
 # <https://wiki.archlinux.org/title/PAM>
-# Enable file-level encryption on login
+
+# ... pam_mount isn't in Alpine. cf.
+# <https://gitlab.alpinelinux.org/alpine/aports/-/issues/13758>
+# TODO
+exit 1
 
 grep -q alpine /etc/os-release || exit 1
+test -z "$(ls -A $HOME)"
 
-doas apk add linux-pam
-
-# TODO
-
-# <volume user="$USER" fstype="fuse" options="nodev,nosuid,quiet"
-# path="/usr/local/bin/gocryptfs#/home/%(USER).cipher" mountpoint="/home/%(USER)" />
+# doas apk add linux-pam gocryptfs
+# doas mkdir /home/$USER.cipher
+# doas chown $USER /home/$USER.cipher
+# gocryptfs -init /home/$USER.cipher
