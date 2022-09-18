8e6fdf8a41cd753e7be6530d4ddf623bccff325d
nsv <nsv@valk.ws>
2022-09-18 18:52:25 +0200

    Add luks to setup-disks and remove ecryptfs

--- .setup/01-setup-disk.alpine.sh
+++ .setup/01-setup-disk.alpine.sh
@@ -1,5 +1,8 @@
 #!/bin/ash
-set -e
+# Dual (or triple) boot 
+# <https://wiki.archlinux.org/title/Dm-crypt>
+# <https://wiki.alpinelinux.org/wiki/LVM_on_LUKS>
+set -eu
 
 DISK="/dev/sda"
 
@@ -8,8 +11,10 @@ apk add parted
 parted --script "$DISK" -- \
     mklabel gpt \
     mkpart \"efi\" fat32 0% 512MiB \
-    mkpart \"root\" ext4 512MiB 10GiB \
-    mkpart \"data\" 10GiB 100% \
+    mkpart \"swap\" swap 512MiB 8GiB \
+    mkpart \"alpine\" ext4 8GiB 24GiB \
+    mkpart \"debian\" ext4 24GiB 40GiB \
+    mkpart \"home\" 40GiB 100% \
     set 1 boot on \
     set 1 esp on
 
@@ -23,3 +28,16 @@ mkdir -p /mnt/boot/efi
 mount "${DISK}1" /mnt/boot/efi
 export BOOTLOADER=grub USE_EFI=1
 setup-disk sys /mnt
+
+DEV=/dev/sda
+
+doas cryptsetup luksFormat -v "$DEV"
+doas cryptsetup open "$DEV" cryptroot
+doas mkfs -t ext4 /dev/mapper/cryptroot
+doas cryptsetup close cryptroot
+
+### To add a key:
+# doas cryptsetup luksAddKey "$DEV" $HOME/.luks/key.bin
+
+### To create a backup:
+# cryptsetup luksHeaderBackup "$DEV" --header-backup-file $HOME/.luks/backup.img
--- .setup/02-encrypt-home-with-luks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# <https://wiki.archlinux.org/title/Dm-crypt>
-# <https://wiki.alpinelinux.org/wiki/LVM_on_LUKS>
-set -eu
-
-# TODO
