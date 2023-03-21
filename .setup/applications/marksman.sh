9825e78e4ecdb603fda8212afa15c4c72e107caa
nsbg <nsbg@mailbox.org>
2023-03-21 16:40:52 +0100

    Add marksman

---
+++ .setup/applications/marksman.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Markdown language server
+# https://github.com/artempyanykh/marksman
+
+wget -O ~/.local/bin/marksman "$( \
+    curl -s 'https://api.github.com/repos/artempyanykh/marksman/releases/latest' \
+    | jq -r '.assets | .[] | .browser_download_url | select(endswith("-linux"))'\
+)"
+chmod +x ~/.local/bin/marksman
