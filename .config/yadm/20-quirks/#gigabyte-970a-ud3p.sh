dff0251bdb042627b76857d7432d10bea998b079
nsbg <nsbg@mailbox.org>
2023-04-24 15:37:50 +0200

    Things missed after installation.

--- .config/yadm/20-quirks/#gigabyte-970a-ud3p.sh
+++ .config/yadm/20-quirks/#gigabyte-970a-ud3p.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Prevents Gigabyte 970A-UD3P motherboard from freaking out.
 
-# First enable IOMMU in the BIOS. Then add iommu=soft (or perhaps
-# amd_iommu=off) to kernel options:
-sed --in-place 's/\(GRUB_CMDLINE_LINUX_DEFAULT\)="\(.*\)"/\1="\2 iommu=soft"/g' /etc/default/grub
+# First enable IOMMU in the BIOS. Then add amd_iommu=on iommu=pt (or iommu=soft 
+# or perhaps amd_iommu=off) to kernel options:
+sed --in-place 's/\(GRUB_CMDLINE_LINUX_DEFAULT\)="\(.*\)"/\1="\2 amd_iommu=on iommu=pt"/g' /etc/default/grub
 sudo update-grub
