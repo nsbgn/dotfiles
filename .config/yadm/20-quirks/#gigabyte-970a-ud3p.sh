1bc8fc34d93a0ef35a323f43f6c30b7e2d2f23fc
nsbg <nsbg@mailbox.org>
2023-04-28 12:07:53 +0200

    Add note on gigabyte

--- .config/yadm/20-quirks/#gigabyte-970a-ud3p.sh
+++ .config/yadm/20-quirks/#gigabyte-970a-ud3p.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 # Prevents Gigabyte 970A-UD3P motherboard from freaking out.
+# See also 
+# <https://unix.stackexchange.com/questions/519758/amd-vi-completion-wait-loop-after-a-failed-install-of-arch-linux-on-a-blank-ssd>
 
 # First enable IOMMU in the BIOS. Then add amd_iommu=on iommu=pt (or iommu=soft 
 # or perhaps amd_iommu=off) to kernel options:
