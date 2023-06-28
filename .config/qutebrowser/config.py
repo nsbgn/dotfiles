95528c956aa5f9e5be7f9e588ffb456b5de9345f
nsbg <nsbg@mailbox.org>
2023-06-28 23:23:18 +0200

    Add qutebrowser config

---
+++ .config/qutebrowser/config.py
@@ -0,0 +1,8 @@
+# pylint: disable=C0111
+c = c  # noqa: F821 pylint: disable=E0602,C0103
+config = config  # noqa: F821 pylint: disable=E0602,C0103
+
+c.tabs.show = "multiple"
+c.tabs.tabs_are_windows = True
+
+config.load_autoconfig(False)
