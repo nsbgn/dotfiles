a05a881ac7996a0356054bcaec5e4bab5afd1da0
nsbg <nsbg@mailbox.org>
2023-11-16 18:28:51 +0100

    Stuff

--- .scripts/i3ipc_tree.py
+++ .scripts/i3ipc_tree.py
@@ -24,11 +24,14 @@ def print_con(con: i3.Con, prefix: str = "") -> None:
         print("\033[0m", end="")
     for node in con.nodes:
         print_con(node, prefix=prefix + " │ ")
+    for node in con.floating_nodes:
+        print_con(node, prefix=prefix + " │ ")
 
 
 if __name__ == "__main__":
     conn = i3.Connection()
     tree: i3.Con = conn.get_tree()
-    for workspace in tree.workspaces():
-        print_con(workspace)
-        print()
+    print_con(tree)
+    # for workspace in tree.workspaces():
+    #     print_con(workspace)
+    #     print()
