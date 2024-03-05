2174e80ac645aa392913fe2b07a96362d0d2bd53
nsbg <nsbg@mailbox.org>
2024-03-05 11:23:44 +0100

    Add organicmaps

---
+++ .config/yadm/50-extra/organicmaps.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Organic maps is an android app with offline hiking and biking maps. There is 
+# an experimental desktop app!
+#
+# https://organicmaps.app/
+# https://mpr.makedeb.org/packages/organicmaps
+
+flatpak install flathub app.organicmaps.desktop
