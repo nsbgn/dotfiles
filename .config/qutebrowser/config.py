7faabc5edb4b0876d4a4124f71f187899acdc793
nsbg <nsbg@mailbox.org>
2024-03-17 23:47:31 +0100

    Hide - qutebrowser

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -5,4 +5,6 @@ config = config  # noqa: F821 pylint: disable=E0602,C0103
 c.tabs.show = "multiple"
 c.tabs.tabs_are_windows = True
 
+c.window.title_format = "{perc}{current_title}"
+
 config.load_autoconfig(False)
