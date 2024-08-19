98b29fcc2fbcc4c1a04075cac5aba36132c75f50
Niels Steenbergen <nsbg@mailbox.org>
2024-08-19 23:10:01 +0200

    Update markdown-oxide

--- .config/yadm/50-extra/markdown-oxide.sh
+++ .config/yadm/50-extra/markdown-oxide.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 # <https://github.com/Feel-ix-343/markdown-oxide>
 
-cargo install markdown-oxide
+# cargo install --locked --git https://github.com/Feel-ix-343/markdown-oxide.git markdown-oxide
+
+PREFIX="${HOME}/.local/bin"
+BIN="${PREFIX}/markdown-oxide"
+if [ ! -f "${BIN}" ]; then
+mkdir -p ${PREFIX}
+wget -O "${BIN}" "$( \
+    curl -fL 'https://api.github.com/repos/Feel-ix-343/markdown-oxide/releases/latest' \
+    | jq -r '.assets[].browser_download_url | select(endswith("x86_64-unknown-linux-gnu"))' \
+)"
+chmod +x "${BIN}"
+fi
