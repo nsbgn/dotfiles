19a78dbc0bcfc672279e4f04277be403b5c19f36
Niels Steenbergen <nsbg@mailbox.org>
2025-03-26 22:51:59 +0100

    Add neotree

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -257,6 +257,23 @@ require("lazy").setup({
       end
     },
 
+    {
+      "https://github.com/nvim-neo-tree/neo-tree.nvim",
+      branch = "v3.x",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+        "MunifTanjim/nui.nvim",
+        -- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+      },
+      lazy = false, -- neo-tree will lazily load itself
+      ---@module "neo-tree"
+      ---@type neotree.Config?
+      opts = {
+        -- fill any relevant options here
+      },
+    },
+
     { 'https://github.com/folke/zen-mode.nvim',
       init = function()
         require("zen-mode").setup {
