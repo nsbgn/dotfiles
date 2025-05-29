cf7166fa94560173a2970985009cae725271510d
Niels Steenbergen <nsbg@mailbox.org>
2025-05-29 14:22:21 +0200

    Update signal

--- .config/yadm/apps/signal.sh
+++ .config/yadm/apps/signal.sh
@@ -2,5 +2,5 @@
 # I'm not a big fan of Signal's approach here. Either Snap or Flatpak or a
 # Debian/Ubuntu repository. Using OpenSUSE's build system for now.
 
-wget https://download.opensuse.org/repositories/network:/im:/signal/Fedora_41/network:im:signal.repo
-dnf config-manager addrepo --from-repofile=network:im:signal.repo
+wget https://download.opensuse.org/repositories/network:/im:/signal/Fedora_42/network:im:signal.repo
+sudo dnf config-manager addrepo --from-repofile=network:im:signal.repo --overwrite
