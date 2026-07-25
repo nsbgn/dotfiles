cedd3f6daedca0a42686152f9b1dad35d6d68376
Niels Steenbergen <nsbg@mailbox.org>
2026-07-25 21:46:44 +0200

    Always set background to pure black or white

--- .config/nvim/lua/colorscheme-gruvbox.lua
+++ .config/nvim/lua/colorscheme-gruvbox.lua
@@ -1,6 +1,7 @@
 vim.pack.add({
   { src = 'https://github.com/ellisonleao/gruvbox.nvim', version = '2.0.0' },
   -- { src = 'https://github.com/sainnhe/gruvbox-material', version = 'v1.2.5' },
+  -- { src = 'https://github.com/typicode/bg.nvim', version = '85da2c68409820466753b50c2e6f699517778a17' },
 })
 
 require("gruvbox").setup({
@@ -40,5 +41,21 @@ vim.api.nvim_set_hl(0, "@markup.heading.1", { underdouble = true, bold = true })
 vim.api.nvim_set_hl(0, "@markup.heading.2", { underline = true, bold = true, italic = true })
 vim.api.nvim_set_hl(0, "@markup.heading.3", { underdotted = true, italic = true })
 vim.api.nvim_set_hl(0, "@markup.heading.4", { underdotted = true })
-vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
-vim.api.nvim_set_hl(0, "SignColumn", { bg = "NONE" })
+
+-- Always set background to pure black or white
+local update_bg = function(ev)
+  if vim.o.background == 'light' then
+    bg = 'white'
+  else
+    bg = 'black'
+  end
+  vim.api.nvim_set_hl(0, "Normal", { bg = bg })
+  vim.api.nvim_set_hl(0, "SignColumn", { bg = bg })
+end
+vim.api.nvim_create_autocmd({'OptionSet'}, {
+  pattern = 'background',
+  callback = update_bg
+})
+vim.api.nvim_create_autocmd({'ColorScheme'}, {
+  callback = update_bg
+})
